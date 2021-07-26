f5fcc28ab4530452c08ffc39122f5d71d25c3c70
JanekJaran <janek.tepsa@gmail.com>
2021-07-26 19:28:54 +0200

    Dodanie strony z detalami o miescie i przekierowania do niej z wyszukiwarki.

--- app1/urls.py
+++ app1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 # from . import views
-from .views import CityViewSet, CitySearchView, CityListView, CityListCreateView
+from .views import CityViewSet, CitySearchView, CityListView, CityListCreateView, CityDetailsView
 from rest_framework.routers import DefaultRouter
 
 
@@ -19,4 +19,5 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(
         redirect_authenticated_user=True, template_name='login.html', ), name='u-login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/'), name='u-logout'),
+    path('city/<int:id>/', CityDetailsView.as_view())
     ]
